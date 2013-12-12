f592d87ea1fc3db416ec9ede336a4071d8a568cf
sirloins <sirloin@everux.com>
2013-12-12 13:43:59 +0700

    Added 'auth_key/auth_secret' credentials handling. Added 'requester' class.
    Credentials are now required by MtGox in order to access the API.
    You will need to open an account with MtGox and obtain your credentials (key and secret) from the security section in your MtGox settings.  You do not need to be verified for trading to qualify for these credentials.

--- mtgoxticker.py
+++ mtgoxticker.py
@@ -5,20 +5,56 @@ import json
 import sys
 import time
 import traceback
+from urllib import urlencode
 import urllib2
+from hashlib import sha512
+from hmac import HMAC
+import base64
 
 FULLTICKER = True     # If False then display only the weighted average price
+auth_key = 'YOUR.AUTH.KEY.FROM.MTGOX'
+auth_secret = 'YOUR.AUTH.SECRET.FROM.MTGOX'
+
+def get_nonce():
+    return int(time.time()*100000)
+ 
+def sign_data(secret, data):
+    return base64.b64encode(str(HMAC(secret, data, sha512).digest()))
+ 
+class requester:
+    def __init__(self, auth_key, auth_secret):
+        self.auth_key = auth_key
+        self.auth_secret = base64.b64decode(auth_secret)
+ 
+    def build_query(self, req={}):
+        req["nonce"] = get_nonce()
+        post_data = urlencode(req)
+        headers = {}
+        headers["User-Agent"] = "GoxApi"
+        headers["Rest-Key"] = self.auth_key
+        headers["Rest-Sign"] = sign_data(self.auth_secret, post_data)
+        return (post_data, headers)
+ 
+    def perform(self, path, args):
+        data, headers = self.build_query(args)
+        req = urllib2.Request("https://mtgox.com/api/1/"+path, data, headers)
+        res = urllib2.urlopen(req, data)
+        return json.load(res)
 
 class _BitcoinTicker():
   """ Retrieve ticker data (json format) from mtgox.com and convert it to plain html.
       The resulting ticker is similar to the one displayed on the Mt. Gox
       main page.
   """
-  def main(self):
+  def main(self, auth_key, auth_secret):
     try:
       print 'Content-type: text/html\n'
-      handle = urllib2.urlopen('https://mtgox.com/api/1/BTCUSD/ticker')
-      data = json.load(handle)
+      reqr = requester(auth_key, auth_secret)
+      args = {}
+      data = reqr.perform('BTCUSD/ticker', args)
+      #@@@ earlier code had a way to specify 8 seconds timeout
+      #handle = urllib2.urlopen('https://mtgox.com/api/1/BTCUSD/ticker', timeout=8)
+      #data = json.load(handle)
       if data['result'] == 'success':
         last = data['return']['last']
         high = data['return']['high']
@@ -30,18 +66,18 @@ class _BitcoinTicker():
         vol_display = vol['display_short']
         if len(volparts) > 1:
           vol_display = volparts[0]
-        spacer = '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'
+        spacer = '&nbsp;&nbsp;&nbsp;&nbsp;'
         if FULLTICKER:
-          print 'Last price:%s%s High:%s%s Low:%s%s Volume:%s BTC%s Weighted Avg:%s USD'  % (
-              last['display_short']
+          print 'Last %s%s High %s%s Low %s%s Volume %s BTC%s Weighted Avg %s USD'  % (
+              last['display_short'].replace('$','')
             , spacer
-            , high['display_short']
+            , high['display_short'].replace('$','')
             , spacer
-            , low['display_short']
+            , low['display_short'].replace('$','')
             , spacer
             , vol_display
             , spacer
-            , vwap['display_short']
+            , vwap['display_short'].replace('$','')
           )
         else:
           print '1.00 BTC = %s %s (Weighted Average) as of %s %s' % (vwap['display_short'], vwap['currency'], time.strftime('%Y.%m.%d %H:%M:%S'), time.tzname)
@@ -63,4 +99,4 @@ class _BitcoinTicker():
 
 if __name__ == '__main__':
   app = _BitcoinTicker()
-  app.main()
+  app.main(auth_key, auth_secret)
